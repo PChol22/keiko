aaee10414a680856a74deed2c303aa20a4253bf8
Pierre Chollet <pchol@MacBook-Pro-de-Pierre.local>
2022-05-02 15:43:25 +0200

    My first pokemon

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,8 +3,9 @@ import styles from "./Home.module.css"
 export const Home = () => {
   return (
     <div className={styles.intro}>
-      <div>Bienvenue sur ton futur pokédex !</div>
-      <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt="Carapuce" />
+      <p>Carapuce</p>
+      <p>Numéro 7</p>
     </div>
   )
 }
