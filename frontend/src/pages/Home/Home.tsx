6a9a194d6b0196901acc223e73a4554e5b142fe9
Pierre Chollet <pchol@MacBook-Pro-de-Pierre.local>
2022-05-02 18:38:24 +0200

    add error handler

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -18,13 +18,15 @@ const filterPokemonsByName = (pokemons: PokemonInfo[], filterValue: string): Pok
 
 const fetchPokemons = async (): Promise<PokemonInfo[]> => {
   const response = await fetch("http://localhost:8000/pokemons", { headers: { accept: "Application/json" } })
+  // throw "We have a problem..."
   return response.json()
 }
 
 export const Home = () => {
-  //const [filterValue, setFilterValue] = React.useState("")
+  // const [filterValue, setFilterValue] = React.useState("")
   const [pokemons, updatePokemonsList] = React.useState<PokemonInfo[]>([])
   const [isLoading, setIsLoading] = React.useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = React.useState<string>("")
 
   /*
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,29 +36,34 @@ export const Home = () => {
 
   React.useEffect(() => {
     setIsLoading(true)
-    fetchPokemons().then((pokemons: PokemonInfo[]) => {
-      updatePokemonsList(pokemons)
-      setTimeout(() => setIsLoading(false), 1000)
-    })
+    fetchPokemons()
+      .then((pokemons: PokemonInfo[]) => {
+        updatePokemonsList(pokemons)
+        setTimeout(() => {
+          setIsLoading(false)
+        }, 1000)
+      })
+      .catch((error: string) => setErrorMessage(error))
   }, [])
 
   return (
     <div className={styles.intro}>
       <p className={styles.title}>Pokedex !</p>
       {/*<input className={styles.input} onChange={onInputChange} value={filterValue}></input>*/}
-      {!isLoading && (
+      {!isLoading && !errorMessage && (
         <div className={styles["pokemon-list"]}>
           {
             /*filterPokemonsByName(pokemons, filterValue).map(({ name, id, height, weight }) => (
-            <Pokemon name={name} id={id} height={height} weight={weight} key={id} />
-          ))*/
+          <Pokemon name={name} id={id} height={height} weight={weight} key={id} />
+        ))*/
             pokemons.map(({ name, id, height, weight }) => (
               <Pokemon name={name} id={id} height={height} weight={weight} key={id} />
             ))
           }
         </div>
       )}
-      {isLoading && <Loader />}
+      {isLoading && !errorMessage && <Loader />}
+      {errorMessage && <p>Error : {errorMessage}</p>}
     </div>
   )
 }
