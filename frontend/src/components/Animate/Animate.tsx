84aae663c7553f63f0608b5f55bb62ac6f6a8a2e
Pierre Chollet <pchol@MacBook-Pro-de-Pierre.local>
2022-05-03 12:15:57 +0200

    Add animation to pokemon cards

--- frontend/src/components/Animate/Animate.tsx
+++ frontend/src/components/Animate/Animate.tsx
@@ -1,1 +1,27 @@
-export const Animate = () => <></>
+import { useState } from "react"
+import styles from "./Animate.module.css"
+
+export const Animate = <P extends object>(BaseComponent: React.ComponentType<P>) => {
+  const animatedComponent = (props: P) => {
+    const WithStateComponent = () => {
+      const [className, setClassName] = useState("")
+
+      const addAnimation = () => {
+        setClassName(styles.tadaAnimation)
+      }
+
+      const removeAnimation = () => {
+        setClassName("")
+      }
+
+      return (
+        <div className={className} onMouseEnter={addAnimation} onMouseLeave={removeAnimation}>
+          {<BaseComponent {...props} />}
+        </div>
+      )
+    }
+    return <WithStateComponent />
+  }
+  animatedComponent.displayName = `Animated ${BaseComponent.displayName}`
+  return animatedComponent
+}
